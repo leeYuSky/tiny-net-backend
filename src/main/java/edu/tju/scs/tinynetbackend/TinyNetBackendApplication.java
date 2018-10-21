af13b524944824fe03e783f1d3b2b1d46529dd6a
liyuze <409654850@qq.com>
2018-10-21 21:31:12 +0800

    Initial commit

---
+++ src/main/java/edu/tju/scs/tinynetbackend/TinyNetBackendApplication.java
@@ -0,0 +1,12 @@
+package edu.tju.scs.tinynetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyNetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyNetBackendApplication.class, args);
+    }
+}
